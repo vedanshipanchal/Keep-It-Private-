b9caadc55ea86936c2641d0e130bc16632f5e1bd
vedanshipanchal <vedanshipanchal@gmail.com>
2024-12-13 16:25:13 +0000

    keep it private!

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 class x:
   pin  = 1376
-  def __private(self):
+  def private(self):
     print("I am inside class! i am prodected  using 2underscores")
   def display(self):
-    print("i am inside class without protection!",x.__private)
+    print("i am inside class without protection!",x.private)
 
 
 #object creation
 ob1 = x()
-ob1.__private()
+ob1.private()
 ob1.display()
 print(ob1.pin)
